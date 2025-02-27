5db87616b0ade7fae487443c58c4a95669578518
Worcnaz-Kyouma <nicolasaprado028@gmail.com>
2025-02-26 21:55:07 -0300

    feat: implementing PlayerController

---
+++ GetDownWithHer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private PlayerControls playerControls;
+
+    private void Awake() {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable() {
+        playerControls.Enable();
+    }
+
+    private void OnDisable() {
+        playerControls.Disable();
+    }
+}
