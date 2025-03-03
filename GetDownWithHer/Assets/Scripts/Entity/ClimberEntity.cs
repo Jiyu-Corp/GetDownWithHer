5f4e203878231b35efec4871f234c88d15caa170
Nicolas Prado <nicolasaprado028@gmail.com>
2025-03-03 17:45:55 -0300

    feat: implemented MoveHands proxy to move nested hands

--- GetDownWithHer/Assets/Scripts/Entity/ClimberEntity.cs
+++ GetDownWithHer/Assets/Scripts/Entity/ClimberEntity.cs
@@ -4,15 +4,31 @@ using System;
 using System.Threading.Tasks;
 
 public class ClimberEntity : Entity {
-    protected Collider2D collider;
+    GameObject lHand;
+    GameObject rHand;
 
     void Start() {
-        collider = GetComponent<Collider2D>();
-        Collider2D lHandCollider = transform.Find("LeftHand").GetComponent<Collider2D>();
-        Collider2D rHandCollider = transform.Find("RightHand").GetComponent<Collider2D>();
+        lHand = transform.Find("LeftHand").gameObject;
+        rHand = transform.Find("RightHand").gameObject;
+
+        AdjustBodyColliders();
+    }
+
+    private void AdjustBodyColliders() {
+        Collider2D collider = GetComponent<Collider2D>();
+        Collider2D lHandCollider = lHand.GetComponent<Collider2D>();
+        Collider2D rHandCollider = rHand.GetComponent<Collider2D>();
 
         Physics2D.IgnoreCollision(collider, lHandCollider);
         Physics2D.IgnoreCollision(collider, rHandCollider);
         Physics2D.IgnoreCollision(lHandCollider, rHandCollider);
+    }
+
+    public void MoveHands(Vector2D position) {
+        Hand lHandScript = lHand.GetComponent<Hand>();
+        Hand rHandScript = rHand.GetComponent<Hand>();
+
+        lHandScript.MoveHandToPosition(position);
+        rHandScript.MoveHandToPosition(position);
     }
 }
