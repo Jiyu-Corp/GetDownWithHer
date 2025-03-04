a3bdce2bb9a50ba8c1351fd6cd90414e4976212f
Worcnaz-Kyouma <nicolasaprado028@gmail.com>
2025-03-04 18:33:22 -0300

    feat: prototyping the Hand ApplySpeed logic

--- GetDownWithHer/Assets/Scripts/Entity/Hand.cs
+++ GetDownWithHer/Assets/Scripts/Entity/Hand.cs
@@ -1,4 +1,6 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Hand : MonoBehaviour {
     public float handSpeed = 20f;
@@ -10,6 +12,8 @@ public class Hand : MonoBehaviour {
     private Rigidbody2D rb;
     private DistanceJoint2D dj;
 
+    private Vector2 directionPointer; // Where mouse is, maybe should be on another script? Maybe Climb?
+
     void Start() {
         rb = GetComponent<Rigidbody2D>();
         climberEntity = GetComponentInParent<ClimberEntity>();
@@ -17,8 +21,23 @@ public class Hand : MonoBehaviour {
     }
 
     void FixedUpdate() {
-        Rigidbody2D rbCE = climberEntity.GetComponent<Rigidbody2D>();
-        rb.linearVelocity = rbCE.linearVelocity;
+        ApplySpeed();
+    }
+
+    private void ApplySpeed() {
+        Vector2 speed = new Vector2(0,0);
+        if(!isHolding && IsOnShoulderLimitDistance()) { // the isHolding apply to both hands, in other words, it needs to be on the entity, just like this here we would know if the other hand is holding
+            // Works on directionPointer, mensurate its true module, they parse it removing the cos of the module
+        } else {
+            // just apply the direction pointer speed, cause it is holding or not on limit
+        }
+
+        if(!isHolding) { //To make the hand follow the body, to avoid inertia to stop it
+            Rigidbody2D rbCE = climberEntity.GetComponent<Rigidbody2D>();
+            speed += rbCE.linearVelocity;
+        }
+
+        rb.linearVelocity = speed;
     }
 
     public void MoveHandToPosition(Vector2 position) {
@@ -35,7 +54,7 @@ public class Hand : MonoBehaviour {
     }
 
     public void PrepareHold(bool enable) {
-        isPreparedToHold = enabled;
+        isPreparedToHold = enable;
     }
 
     public void StopHold() {
