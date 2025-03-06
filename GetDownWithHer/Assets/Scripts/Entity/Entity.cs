79a806a86ddb2b5139abfc5b8441babdab3b4358
Worcnaz-Kyouma <nicolasaprado028@gmail.com>
2025-03-06 17:12:00 -0300

    feat: Mov V2

--- GetDownWithHer/Assets/Scripts/Entity/Entity.cs
+++ GetDownWithHer/Assets/Scripts/Entity/Entity.cs
@@ -1,144 +1,63 @@
-using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
 
-// HP still not implemented
 public class Entity : MonoBehaviour {
     [Header("Movement Settings")]
-    public float acceleration = 24;
     public float moveSpeed = 7f;
-    public float boastedMoveSpeed = 14f;
-    public float timeUntilReachBoastedSpeed = 2f;
     public float jumpForce = 7f;
 
     protected Rigidbody2D rb;
 
-    // Terrain states
-    protected bool inGround = false;
-    protected bool inWall = false;
-
-    protected bool isMoving = false;
-    protected bool isBoastedMoving = false;
-    private float movingDirection = 0f;
-    private float timeSinceStartMoving = 0f;
-
-    private Task cachedStartMove = null;
-    private Task cachedStopMove = null;
+    private float horizontalMoveDirection = 0f;
 
     private Dictionary<Collider2D, Vector2> currentCollidingNormals = new Dictionary<Collider2D, Vector2>();
+
+    // Terrain states
+    private bool inGround = false;
 
     protected virtual void Awake() {
         rb = GetComponent<Rigidbody2D>();
     }
 
     protected void FixedUpdate() {
-        if(isMoving && !inWall) {
-            isBoastedMoving = timeSinceStartMoving > timeUntilReachBoastedSpeed;
+        PlayerMove();
+    }
+
+    private void PlayerMove() {
+        bool canPlayerChangeEntitySpeed = horizontalMoveDirection != 0f && inGround;
+        if(canPlayerChangeEntitySpeed) {
+            const float defaultAcceleration = 16f;
 
             rb.linearVelocity = new Vector2(Mathf.MoveTowards(
                 rb.linearVelocity.x,
-                movingDirection * (isBoastedMoving ? boastedMoveSpeed : moveSpeed),
-                acceleration * Time.fixedDeltaTime
-            ), rb.linearVelocity.y);
-
-            timeSinceStartMoving += Time.fixedDeltaTime;
-        } else if (inWall) {
-            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
-        } else {
-            timeSinceStartMoving = 0f;
-            isBoastedMoving = false;
-
-            rb.linearVelocity = new Vector2(Mathf.MoveTowards(
-                rb.linearVelocity.x,
-                0,
-                acceleration/3 * Time.fixedDeltaTime
+                horizontalMoveDirection * moveSpeed,
+                defaultAcceleration * Time.fixedDeltaTime
             ), rb.linearVelocity.y);
         }
     }
 
-    public virtual void StartMove(float horizontalDirection) {
-        bool isStartMoveSetupSuccessful = SetupStartMove(horizontalDirection);
-        
-        if(!isStartMoveSetupSuccessful) {
-            if(cachedStopMove != null) cachedStopMove = null;
-            cachedStartMove = StartMoveAsync(horizontalDirection);
-        }
-    }
-    private bool SetupStartMove(float horizontalDirection) {
-        if(inGround) {
-            isMoving = true;
-            movingDirection = horizontalDirection;
-        }
-
-        return inGround;
-    }
-    private Task StartMoveAsync(float horizontalDirection) {
-        return Task.Run(() => {
-            while(cachedStartMove != null) {
-                bool isStartMoveSetupSuccessful = SetupStartMove(horizontalDirection);
-                if(isStartMoveSetupSuccessful) cachedStartMove = null;
-
-                Task.Yield();
-            }
-        });
-    }
-
-    public virtual void StopMove(bool verifyInGround = true) {
-        bool isStopMoveSetupSuccessful = SetupStopMove(verifyInGround);
-
-        if(!isStopMoveSetupSuccessful) {
-            if(cachedStartMove != null) cachedStartMove = null;
-            cachedStopMove = StopMoveAsync(verifyInGround);
-        }
-    }
-    private bool SetupStopMove(bool verifyInGround) {
-        if(!verifyInGround || inGround) {
-            isMoving = false;
-            movingDirection = 0f;
-        }
-
-        return !verifyInGround || inGround;
-    }
-    private Task StopMoveAsync(bool verifyInGround) {
-        return Task.Run(() => {
-            while(cachedStopMove != null) {
-                bool isStopMoveSetupSuccessful = SetupStopMove(verifyInGround);
-                if(isStopMoveSetupSuccessful) cachedStopMove = null;
-                Task.Yield();
-            }
-        });
-    }
-
-    /// <summary>
-    /// Moves the entity upwards the y axis if on ground
-    /// </summary>
     public virtual void Jump() {
         if(inGround) {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
     }
 
-    private void ValidateAndUpdateTerrainStates() {
+    public void ComposeMoveDirection(float direction, bool addDirection) {
+        horizontalMoveDirection += direction * (addDirection ? 1 : -1);
+    }
+
+    protected void ValidateAndUpdateTerrainStates() {
         float minYNormalForInGroundCheck = 0.3f;
-        float minXNormalForInWallCheck = 0.75f;
         
         bool isInGroundDetected = false;
-        bool isInWallDetected = false;
         foreach(Vector2 normal in currentCollidingNormals.Values) {
             if(!isInGroundDetected && normal.y >= minYNormalForInGroundCheck) {
                 isInGroundDetected = true;
             }
-
-            if(!isInWallDetected && Math.Abs(normal.x) >= minXNormalForInWallCheck) {
-                isInWallDetected = true;
-            }
         }
 
         inGround = isInGroundDetected;
-        inWall = isInWallDetected;
     }
-
     protected virtual void OnCollisionEnter2D(Collision2D collision) {
         foreach(ContactPoint2D contact in collision.contacts) {
             currentCollidingNormals[contact.collider] = contact.normal;
--- GetDownWithHer/Assets/Scripts/PlayerScripts/PlayerInputController.cs
+++ GetDownWithHer/Assets/Scripts/PlayerScripts/PlayerInputController.cs
@@ -5,17 +5,21 @@ public class PlayerInputController : MonoBehaviour {
     public Camera mainCamera;
 
     private PlayerInput playerInput;
-    private ClimberEntity climberEntity;
+    private Entity entity;
 
     // Actions
     private InputAction jumpAction;
-    private InputAction moveAction;
+    //private InputAction moveAction; This was removed cause multiple bind call to same action was returning wrong value, on the canceled event
+    private InputAction moveRightAction;
+    private InputAction moveLeftAction;
     private InputAction holdHandL;
     private InputAction holdHandR;
 
     private void Awake() {
         playerInput = GetComponent<PlayerInput>();
-        climberEntity = GetComponent<ClimberEntity>();
+        entity = GetComponent<Entity>();
+
+        // InputSystem.settings.SetInternalFeatureFlag("DISABLE_SHORTCUT_SUPPORT", true); This was used to fix multiple bind call in vector... but didnt solve
 
         InitActions();
         BindHandlersToActions();
@@ -23,22 +27,29 @@ public class PlayerInputController : MonoBehaviour {
 
     private void InitActions() {
         jumpAction = playerInput.actions["Jump"];
-        moveAction = playerInput.actions["Move"];
+        //moveAction = playerInput.actions["Move"];
+        moveRightAction = playerInput.actions["MoveRight"];
+        moveLeftAction = playerInput.actions["MoveLeft"];
         holdHandL = playerInput.actions["HoldHandL"];
         holdHandR = playerInput.actions["HoldHandR"];
     }
 
     private void BindHandlersToActions() {
-        jumpAction.performed += ctx => climberEntity.Jump();
-        moveAction.started += ctx => climberEntity.StartMove(ctx.ReadValue<Vector2>().x);
-        moveAction.canceled += ctx => climberEntity.StopMove();
+        jumpAction.performed += ctx => entity.Jump();
+        moveRightAction.started += ctx => entity.ComposeMoveDirection(1f, true);
+        moveRightAction.canceled += ctx => entity.ComposeMoveDirection(1f, false);
+        moveLeftAction.started += ctx => entity.ComposeMoveDirection(-1f, true);
+        moveLeftAction.canceled += ctx => entity.ComposeMoveDirection(-1f, false);
 
-        const int L_HAND = 0;
-        const int R_HAND = 1;
-        holdHandL.started += ctx => climberEntity.ManageHoldHand(L_HAND, true);
-        holdHandL.canceled += ctx => climberEntity.ManageHoldHand(L_HAND, false);
-        holdHandR.started += ctx => climberEntity.ManageHoldHand(R_HAND, true);
-        holdHandR.canceled += ctx => climberEntity.ManageHoldHand(R_HAND, false);
+        ClimberEntity cEntity = entity as ClimberEntity;
+        if(cEntity != null) {
+            const int L_HAND = 0;
+            const int R_HAND = 1;
+            holdHandL.started += ctx => cEntity.ManageHoldHand(L_HAND, true);
+            holdHandL.canceled += ctx => cEntity.ManageHoldHand(L_HAND, false);
+            holdHandR.started += ctx => cEntity.ManageHoldHand(R_HAND, true);
+            holdHandR.canceled += ctx => cEntity.ManageHoldHand(R_HAND, false);
+        }
     }
 
     private void FixedUpdate() {
@@ -46,7 +57,10 @@ public class PlayerInputController : MonoBehaviour {
     }
 
     private void UpdatePlayerDirectionPointer() {
+        ClimberEntity cEntity = entity as ClimberEntity;
+        if(cEntity == null) return;
+
         Vector2 newPointerPositionOnWorldSpace = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        climberEntity.SetDirectionPointer(newPointerPositionOnWorldSpace);
+        cEntity.SetDirectionPointer(newPointerPositionOnWorldSpace);
     }
 }
