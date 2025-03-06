db7a18ad553a9c43a61fc297b2b75456ae581e7a
Worcnaz-Kyouma <nicolasaprado028@gmail.com>
2025-03-06 11:37:31 -0300

    fix: Update of pointer position in world space

--- GetDownWithHer/Assets/Scripts/PlayerScripts/PlayerInputController.cs
+++ GetDownWithHer/Assets/Scripts/PlayerScripts/PlayerInputController.cs
@@ -10,7 +10,6 @@ public class PlayerInputController : MonoBehaviour {
     // Actions
     private InputAction jumpAction;
     private InputAction moveAction;
-    private InputAction moveHandsAction;
     private InputAction holdHandL;
     private InputAction holdHandR;
 
@@ -25,7 +24,6 @@ public class PlayerInputController : MonoBehaviour {
     private void InitActions() {
         jumpAction = playerInput.actions["Jump"];
         moveAction = playerInput.actions["Move"];
-        moveHandsAction = playerInput.actions["MoveHands"];
         holdHandL = playerInput.actions["HoldHandL"];
         holdHandR = playerInput.actions["HoldHandR"];
     }
@@ -35,13 +33,20 @@ public class PlayerInputController : MonoBehaviour {
         moveAction.started += ctx => climberEntity.StartMove(ctx.ReadValue<Vector2>().x);
         moveAction.canceled += ctx => climberEntity.StopMove();
 
-        moveHandsAction.performed += ctx => climberEntity.SetDirectionPointer(mainCamera.ScreenToWorldPoint(ctx.ReadValue<Vector2>()));
-
         const int L_HAND = 0;
         const int R_HAND = 1;
         holdHandL.started += ctx => climberEntity.ManageHoldHand(L_HAND, true);
         holdHandL.canceled += ctx => climberEntity.ManageHoldHand(L_HAND, false);
         holdHandR.started += ctx => climberEntity.ManageHoldHand(R_HAND, true);
         holdHandR.canceled += ctx => climberEntity.ManageHoldHand(R_HAND, false);
-    } 
+    }
+
+    private void Update() {
+        UpdatePlayerDirectionPointer();
+    }
+
+    private void UpdatePlayerDirectionPointer() {
+        Vector2 newPointerPositionOnWorldSpace = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        climberEntity.SetDirectionPointer(newPointerPositionOnWorldSpace);
+    }
 }
